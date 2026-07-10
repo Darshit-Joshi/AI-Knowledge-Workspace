4259537c54b9e08427fcb126ce5ba3ab218498ec
Prachi Ahlawat <chaudhary.prachi945@gmail.com>
2026-07-10 22:28:34 +0530

    Fix user model, auth schema, and add backend utils

--- backend/schemas/auth.py
+++ backend/schemas/auth.py
@@ -1,14 +1,18 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr , Field
 from typing import Optional
 
 class UserRegister(BaseModel):
-  email : EmailStr
-  username : str
-  password : str
-  
+    email: EmailStr
+    username: str = Field(min_length=3, max_length=50)
+    password: str = Field(min_length=8)
+
 class UserLogin(BaseModel):
-  email: EmailStr
-  password : str
+    email: EmailStr
+    password: str = Field(min_length=8)
+
+class ResetPasswordRequest(BaseModel):
+    token: str
+    new_password: str = Field(min_length=8)
   
 class TokenResponse(BaseModel):
   access_token: str
@@ -16,3 +20,5 @@ class TokenResponse(BaseModel):
   user_email: str
   auth_provider : str
   
+class ForgotPasswordRequest(BaseModel):
+  email: EmailStr
